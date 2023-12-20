a12a2a8d120a0aac933228beb4761e684b3a117f
Michael Alvarez <Michael.alv.dev@gmail.com>
2023-12-19 23:23:54 -0400

    Cosntruccion footer, estilos y enlaces

---
+++ src/componentes/Footer.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import styles from "./footer.module.css"
+import PokemonPic from "../assets/pikachu.png";
+import LocationPic from "../assets/pointer.png";
+import ItemPic from "../assets/pokeball.png";
+
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <Link to="/pokemons" className={styles.footerLink}>
+                <img className={styles.footerIcon} src={PokemonPic} alt="Pokeball" />
+                Pokemons
+            </Link>
+            <Link to="/items" className={styles.footerLink}>
+                <img className={styles.footerIcon} src={ItemPic} alt="Pokeball" />
+                Items
+            </Link>
+            <Link to="/map" className={styles.footerLink}>
+                <img className={styles.footerIcon} src={LocationPic} alt="Pokeball" />
+                Map
+            </Link>
+        </footer>
+    );
+}
+
+export default Footer;
