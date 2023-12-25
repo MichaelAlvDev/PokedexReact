d30ccc08c3ab106b451bde66781a497ed84f1aac
Michael Alvarez <Michael.alv.dev@gmail.com>
2023-12-25 19:24:38 -0400

    Maqueta pagina detalles Pokemon(individual) con estilos css

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,15 +1,37 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Footer from "../componentes/Footer";
-
+import PokeballImg from "../assets/pokeball.png"
+import PokeMain from "../assets/loading.gif"
+import styles from "./pokemon.module.css"
 const Pokemon = () => {
     const { name } = useParams();
-
+    const navigate = useNavigate();
     return (
-    <div>
-        {name}
-        <Footer />
-    </div>
-)
+        <>
+            <button
+            className={styles.button}
+                onClick={() => navigate(-1)}>
+                <img 
+                className={styles.buttonImg}
+                src={PokeballImg} 
+                alt="Go-back" 
+                />Go Back
+            </button>
+            <div className={styles.pokemon}>
+                <main className={styles.pokemonInfo}>
+                    <div className={styles.pokemonTitle}>{name?.toUpperCase()}</div>
+                    <div>Nro 001{/*{Pokemon.id}*/}</div>
+                    <div>
+                        <img className={styles.pokemonInfoImg} src={PokeMain} alt={Pokemon.name} />
+                    </div>
+                    <div>Hp: 80</div>
+                    <div>Attack: 20</div>
+                    <div>Defense:40</div>
+                </main>
+            </div>
+            <Footer />
+        </>
+    )
 };
 
 export default Pokemon;
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -15,7 +15,7 @@ const Pokemons = () => {
     useEffect(() => {
         const fetchAllPokemons = async () => {
             setIsLoading(true);
-            await waitFor(1000);
+            await waitFor(600);
             const allPokemons = await fetchPokemons();
             setPokemons(allPokemons);
              setIsLoading(false);
