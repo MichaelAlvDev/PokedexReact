690a3e1e6994ca949539b091e7d3defb8e858a35
Michael Alvarez <Michael.alv.dev@gmail.com>
2023-12-25 23:06:00 -0400

    Paginas Coming Soon

--- src/pages/Items.tsx
+++ src/pages/Items.tsx
@@ -1,12 +1,47 @@
-import Footer from "../componentes/Footer";
+import { useNavigate } from "react-router-dom";
+import Footer from '../componentes/Footer';
+import styles from "./items.module.css"
+import PokeballImg from "../assets/pokeball.png"
+import comingSoon from "../assets/loading.gif"
+import { useEffect, useState } from "react";
+import LoadingScreen from "../componentes/LoadingScreen";
+import { waitFor } from "../utils/utils";
 
 const Items = () => {
-    return (
-        <div>
-            Items Component
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+    useEffect(() => {
+      async function LoadingScreen(){
+            setIsLoading(true);
+            await waitFor(800);
+            setIsLoading(false);
+            return
+        };
+        LoadingScreen();
+    }, [])
 
+    if (isLoading || !Items) {
+        return <LoadingScreen />
+    }
+    return (
+        <>
+            <button
+                className={styles.button}
+                onClick={() => navigate(-1)}>
+                <img
+                    className={styles.buttonImg}
+                    src={PokeballImg}
+                    alt="Go-back"
+                />Go Back
+            </button>
+            <main >
+                <div className={styles.comingSoon}>
+                    <img src={comingSoon} alt="" className={styles.comingSoonImg} />
+                    <span>Coming soon. . .</span>
+                </div>
+            </main>
             <Footer />
-        </div>
+        </>
     );
 };
 
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,12 +1,47 @@
-
+import { useNavigate } from "react-router-dom";
 import Footer from '../componentes/Footer';
+import styles from "./map.module.css"
+import PokeballImg from "../assets/pokeball.png"
+import comingSoon from "../assets/loading.gif"
+import { useEffect, useState } from "react";
+import LoadingScreen from "../componentes/LoadingScreen";
+import { waitFor } from "../utils/utils";
 
 const Map = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+    useEffect(() => {
+      async function LoadingScreen(){
+            setIsLoading(true);
+            await waitFor(800);
+            setIsLoading(false);
+            return
+        };
+        LoadingScreen();
+    }, [])
+
+    if (isLoading || !Map) {
+        return <LoadingScreen />
+    }
     return (
-        <div>
-            Map pag
+        <>
+            <button
+                className={styles.button}
+                onClick={() => navigate(-1)}>
+                <img
+                    className={styles.buttonImg}
+                    src={PokeballImg}
+                    alt="Go-back"
+                />Go Back
+            </button>
+            <main >
+                <div className={styles.comingSoon}>
+                    <img src={comingSoon} alt="" className={styles.comingSoonImg} />
+                    <span>Coming soon. . .</span>
+                </div>
+            </main>
             <Footer />
-        </div>
+        </>
     );
 };
 
