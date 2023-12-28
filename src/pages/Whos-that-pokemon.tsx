2a5ebc0407030f9729b8ca902a9f7428ba9a3295
Michael Alvarez <Michael.alv.dev@gmail.com>
2023-12-27 20:29:31 -0400

    Correccion errores de renderizado, cambio el manejo del componente a un solo useEffect, y la generacion de del aleatorio desde el evento de los botones

--- src/pages/Whos-that-pokemon.tsx
+++ src/pages/Whos-that-pokemon.tsx
@@ -7,7 +7,6 @@ import RevealImg from "../assets/reveal.png"
 import { useEffect, useState } from "react";
 import LoadingScreen from "../componentes/LoadingScreen";
 import { waitFor } from "../utils/utils";
-import poke from "../assets/charmeleon.png"
 import { Pokemon, chosePokemon, } from "../types/types";
 import { fetchPokemons } from "../api/fetchPokemons";
 
@@ -18,32 +17,35 @@ const WhosThatPokemon = () => {
     const [isHide, setIsHide] = useState(true);
     const [pokemons, setPokemons] = useState<Pokemon[]>([]) //state para array de pokemons
     const [chosenPokemon, setChosenPokemon] = useState({}) //state para el pokemon elegido aleatoriamente
-    const [refreshRandom, setRefreshRandom] = useState(0)
     useEffect(() => { //useEffect para el loading screen y fetch
         async function FetchAllPokemons() {
             setIsLoading(true);
             await waitFor(800);
             const allPokemons = await fetchPokemons();
             setPokemons(allPokemons);
+            const selectPokemon: chosePokemon = allPokemons[Math.floor(Math.random() * pokemons.length)]
+            setChosenPokemon(selectPokemon)
             setIsLoading(false);
             return
         };
         FetchAllPokemons();
+        
     }, [])
+   
     if (isLoading || !WhosThatPokemon) {
         return <LoadingScreen />
     }
-    useEffect(() => { //get random pokemon 
+    function refreshPokemon() { //get random pokemon 
         const selectPokemon: chosePokemon = pokemons[Math.floor(Math.random() * pokemons.length)]
         setChosenPokemon(selectPokemon)
-    }), [refreshRandom]
+    }
 
     function handleClickReveal() {
         isHide ? setIsHide(false) : setIsHide(true)
     }
 
     function handleClickRefresh() {
-        setRefreshRandom(refreshRandom+1)
+        refreshPokemon() 
     }
     return (
         <>
@@ -58,7 +60,7 @@ const WhosThatPokemon = () => {
             </button>
             <main >
                 <div className={styles.pokeHold}>
-                    <img src={poke} alt="" className={`${styles.pokeHoldImg} ${isHide ? styles.silhouette : styles.show}`} />
+                    <img src={chosenPokemon.imgSrcSilhouette} alt="" className={`${styles.pokeHoldImg} ${isHide ? styles.silhouette : styles.show}`} />
                     <span className={`${isHide ? styles.show : styles.hidde}`} >Who`s That Pokemon. . . ?</span>
                     <span className={`${isHide ? styles.hidde : styles.show}`}>It`s . .  <span className={styles.namePokemon}>{chosenPokemon.name}</span>. . .! !</span>
 
